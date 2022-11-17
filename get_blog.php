d676dc0c57240e5a63b0d4d530add8af1d71fca9
Muftinet <learnwithmufti@gmail.com>
2022-11-17 10:53:39 +0000

    init

---
+++ get_blog.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" name="viewport" content="width=device-width" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+</head>
+
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="https://sourcecodester.com">Muftinet</a>
+        </div>
+    </nav>
+    <div class="col-md-3"></div>
+    <div class="col-md-6 well">
+        <h3 class="text-primary" style="color: #FFC300;">PHP/MYSQL - Simple Search Box</h3>
+        <hr style="border-top:1px dotted #ccc;" />
+        <a href="index.php" class="btn btn-success">Back</a>
+        <?php
+        require 'conn.php';
+        if (isset($_REQUEST['id'])) {
+            $query = mysqli_query($conn, "SELECT * FROM `blog` WHERE `blog_id` = '$_REQUEST[id]'") or die(mysqli_error());
+            $fetch = mysqli_fetch_array($query);
+        ?>
+            <h3><?php echo $fetch['title'] ?></h3>
+            <p><?php echo nl2br($fetch['content']) ?></p>
+        <?php
+        }
+        ?>
+
+    </div>
+</body>
+
+</html>
